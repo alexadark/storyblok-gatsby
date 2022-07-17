3dea0830bfb83e7cfffe60304679824394bce0bd
Alexandra Spalato <alexaspalato@gmail.com>
2022-07-17 14:48:06 +0200

    useStoryblokState

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,32 @@
 import * as React from "react";
 import Layout from "../components/Layout";
+import { graphql } from "gatsby";
+import { useStoryblokState } from "gatsby-source-storyblok";
 
 // markup
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  let story = data.storyblokEntry;
+  story = useStoryblokState(story);
+  console.log(story);
+
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>{story.name}</h1>
     </Layout>
   );
 };
 
 export default IndexPage;
+
+export const query = graphql`
+  query HomeQuery {
+    storyblokEntry(full_slug: { eq: "home" }) {
+      content
+      name
+      full_slug
+      uuid
+      id
+      internalId
+    }
+  }
+`;
