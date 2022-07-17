0386a8e600d7f8e1e882ef6fdfbcc23bbeaf4b9d
Alexandra Spalato <alexaspalato@gmail.com>
2022-07-17 13:31:55 +0200

    initialize storyblok API in the global layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,19 @@
 import React from "react";
 import Header from "./Header";
+import { storyblokInit, apiPlugin } from "gatsby-source-storyblok";
+import configuration from "../../gatsby-config";
+
+const sbConfig = configuration.plugins.find(
+  (plugin) => plugin.resolve === "gatsby-source-storyblok"
+);
+
+storyblokInit({
+  accessToken: sbConfig.options.accessToken,
+  use: [apiPlugin],
+  components: {
+    //components
+  },
+});
 
 const Layout = ({ children }) => {
   return (
